db0ec21bea02254961bf2f28e2b76c487e110efa
ArnoldRamodike <riccoarnold23@gmail.com>
2024-07-10 10:50:00 +0200

    Minor error handling

--- KenoGame.API/Program.cs
+++ KenoGame.API/Program.cs
@@ -36,7 +36,12 @@ app.MapGet("api/", () => "Hello World!");
 app.MapGet("games/", () => games);
 
 // Get Game/1
-app.MapGet("games/{id}", (int id) => games.Find(game => game.Id == id))
+app.MapGet("games/{id}", (int id) =>
+{
+    GameDto? game = games.Find(game => game.Id == id);
+
+    return game is null ? Results.NotFound() : Results.Ok(game);
+})
     .WithName(GetGameEndpointName);
 
 // Get Games
@@ -59,6 +64,11 @@ app.MapPost("games/", (CreateGameDto newGame) =>
 app.MapPut("games/{id}", (int id, UpdateGameDto updateGame) =>
 {
     var index = games.FindIndex(game => game.Id == id);
+
+    if (index == -1)
+    {
+        return Results.NotFound();
+    }
 
     games[index] = new GameDto(
         id,
