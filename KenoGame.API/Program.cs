786a8249e1b60f335f49f83b697c7990aa413fef
ArnoldRamodike <riccoarnold23@gmail.com>
2024-07-15 16:17:07 +0200

    Migrations and code start setup. removing warning

--- KenoGame.API/Program.cs
+++ KenoGame.API/Program.cs
@@ -3,11 +3,13 @@ using KenoGame.API.Data;
 
 var builder = WebApplication.CreateBuilder(args);
 
-var connString = "Data Source=GameStore.db";
+var connString = builder.Configuration.GetConnectionString("GameStore");
 builder.Services.AddSqlite<GamesStoreContext>(connString);
 
 var app = builder.Build();
 
 app.MapGaesEndpoints();
+
+app.MigrateDb();
 
 app.Run();
